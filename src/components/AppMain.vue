114d4f2be25bc0fbe566020572e527ca2785bf88
gianmarcocilia <gianmarcocilia97@gmail.com>
2024-02-01 17:33:24 +0100

    add: create component project card

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,19 +1,21 @@
 <script>
 import axios from 'axios';
+import ProjectCard from './ProjectCard.vue';
 
 export default {
     data() {
         return {
             url: 'http://127.0.0.1:8000',
             projects: [],
-        }
+        };
     },
     created() {
         axios.get(`${this.url}/api/projects`).then((resp) => {
             this.projects = resp.data.data;
             console.log(this.projects);
-        })
-    }
+        });
+    },
+    components: { ProjectCard }
 }
 </script>
 
@@ -22,14 +24,7 @@ export default {
         <h2 class="py-3 text-center">I miei progetti</h2>
         <div class="row row-cols-1 row-cols-md-3 row-cols-lg-4 g-3">
             <div class="col" v-for="project in projects" :key="project.id">
-                <div class="card h-100">
-                    <div class="card-body">
-                        <h4>{{ project.title }}</h4>
-                        <img :src="`${url}/storage/${project.project_image}`" alt="">
-                        <p>{{ project.description }}</p>
-                        <p>Tipologia: <strong>{{ project.type ? project.type.name : 'Non definita'}}</strong></p>
-                    </div>
-                </div>
+                <ProjectCard :project="project" :url="url"/>
             </div>
         </div>
     </div>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+    props: {
+        project: Object,
+        url: String
+    },
+    data() {
+        return {
+
+        }
+    },
+}
+</script>
+
+<template>
+    <div class="card h-100">
+        <div class="card-body">
+            <h4>{{ project.title }}</h4>
+            <img :src="`${url}/storage/${project.project_image}`" alt="">
+            <p>{{ project.description }}</p>
+            <p>Tipologia: <strong>{{ project.type ? project.type.name : 'Non definita' }}</strong></p>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
