fe6f025a74429f27a9bc98e264e6975d982bb7fa
gianmarcocilia <gianmarcocilia97@gmail.com>
2024-02-01 16:52:00 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  }
+}
+
+</script>
+
+<template>
+  <h2 class="py-3 text-center">I miei progetti</h2>
+</template>
+
+<style lang="scss">
+@use "./style/general.scss" as *;
+</style>
