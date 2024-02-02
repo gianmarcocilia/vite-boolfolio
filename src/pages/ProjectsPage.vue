faf7b7803344e141d167432ad0395d87c91c5fb4
gianmarcocilia <gianmarcocilia97@gmail.com>
2024-02-02 17:00:58 +0100

    add: laoder for projects page

--- src/pages/ProjectsPage.vue
+++ src/pages/ProjectsPage.vue
@@ -2,28 +2,35 @@
 import axios from 'axios';
 import { store } from '../store';
 import ProjectCard from '../components/ProjectCard.vue';
+import AppLoader from '../components/AppLoader.vue';
 
 export default {
     data() {
         return {
             store,
             projects: [],
+            loading: false
         };
     },
     created() {
+        this.loading = true;
         axios.get(`${this.store.baseUrl}/api/projects`).then((resp) => {
             console.log(resp);
             this.projects = resp.data.data;
+            this.loading = false;
         });
     },
-    components: { ProjectCard }
+    components: { ProjectCard, AppLoader }
 }
 </script>
 
 <template>
     <div class="container">
         <h2 class="py-3 text-center">I miei progetti</h2>
-        <div class="row row-cols-1 row-cols-md-3 row-cols-lg-4 g-3 pb-3">
+        <div v-if="loading">
+            <AppLoader />
+        </div>
+        <div v-else class="row row-cols-1 row-cols-md-3 row-cols-lg-4 g-3 pb-3">
             <div class="col" v-for="project in projects" :key="project.id">
                 <ProjectCard :project="project"/>
             </div>
--- src/pages/SingleProject.vue
+++ src/pages/SingleProject.vue
@@ -1,42 +1,53 @@
 <script>
 import axios from 'axios';
 import { store } from '../store';
+import AppLoader from '../components/AppLoader.vue';
+
 export default {
     data() {
         return {
             store,
             project: null,
+            loading: false
         }
     },
     created() {
+        this.loading = true;
         axios.get(`${store.baseUrl}/api/projects/${this.$route.params.slug}`).then((resp) => {
-        this.project = resp.data;
-      })
-    }
+            this.project = resp.data;
+            this.loading = false;
+        })
+    },
+    components: {
+    AppLoader
+}
 }
 </script>
 
 <template>
-<div class="container" v-if="project">
-    <h2 class="py-3">Stai visualizzando il progetto: {{ project.title }}</h2>
-    <p class="py-1">{{ project.description }}</p>
-    <img v-if="project.project_image" :src="`${store.baseUrl}/storage/${project.project_image}`" alt="">
-    
-    <ul>
-        <li>Tipologia: {{ project.type ? project.type.name : 'Non definita' }}</li>
-        <li v-if="project.technologies.length > 0">
-            Tecnologie:
-            <ul>
-                <li v-for="technology in project.technologies">{{ technology.name }}</li>
-            </ul>
-        </li>
-        <li v-else>Tecnologie non definite.</li>
-    </ul>
+    <div class="container">
+        <div v-if="loading">
+            <AppLoader />
+        </div>
+        <div v-else>
+            <h2 class="py-3">Stai visualizzando il progetto: {{ project.title }}</h2>
+            <p class="py-1">{{ project.description }}</p>
+            <img v-if="project.project_image" :src="`${store.baseUrl}/storage/${project.project_image}`" alt="">
 
-    <p class="fs-6 pt-5">Made by {{ project.user.name }}, email: <strong>{{ project.user.email }}</strong></p>
-</div>
+            <ul>
+                <li>Tipologia: {{ project.type ? project.type.name : 'Non definita' }}</li>
+                <li v-if="project.technologies.length > 0">
+                    Tecnologie:
+                    <ul>
+                        <li v-for="technology in project.technologies">{{ technology.name }}</li>
+                    </ul>
+                </li>
+                <li v-else>Tecnologie non definite.</li>
+            </ul>
+
+            <p class="fs-6 pt-5">Made by {{ project.user.name }}, email: <strong>{{ project.user.email }}</strong></p>
+        </div>
+    </div>
 </template>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
