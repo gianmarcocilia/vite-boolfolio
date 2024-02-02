f511f5f22f02f41a622cac91f26b443faa5decce
gianmarcocilia <gianmarcocilia97@gmail.com>
2024-02-02 15:59:39 +0100

    add: single project page with axios call for api projects slug (wip)

--- src/pages/SingleProject.vue
+++ src/pages/SingleProject.vue
@@ -4,18 +4,21 @@ import { store } from '../store';
 export default {
     data() {
         return {
-            store
+            store,
+            project: null,
         }
     },
     created() {
-        
+        axios.get(`${store.baseUrl}/api/projects/${this.$route.params.slug}`).then((resp) => {
+        this.project = resp.data;
+      })
     }
 }
 </script>
 
 <template>
-<div class="container">
-    <h2>Stai visualizzando il progetto: </h2>
+<div class="container" v-if="project">
+    <h2 class="text-center py-3">Stai visualizzando il progetto: {{ project.title }}</h2>
 
 </div>
 </template>
